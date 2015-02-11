7d42d99018a88742b7563d0c9432f8a00b826ff9
Naufal <naufal_yahaya@hotmail.com>
2015-02-11 09:03:33 -0500

    fixed the bugs for socket.io

--- public/app/controllers/storyCtrl.js
+++ public/app/controllers/storyCtrl.js
@@ -2,54 +2,51 @@
 angular.module('storyCtrl', ['storyService'])
 
 
-.controller('StoryController', function(Story, $routeParams, $scope, socketio) {
+.controller('StoryController', function(Story, $routeParams, socketio) {
 
-	var vm = this;
+    var vm = this;
+    vm.stories = [];
 
-	Story.all()
-	.success(function(data) {
-		vm.stories = data;
-	});
+    Story.all()
+    .success(function(data) {
+        vm.stories = data;
+    });
 
 
-	Story.getSingleStory($routeParams.user_name, $routeParams.story_id)
-	.success(function(data) {
-		vm.storyData = data;
-	});
+    Story.getSingleStory($routeParams.user_name, $routeParams.story_id)
+    .success(function(data) {
+        vm.storyData = data;
+    });
 
-	vm.createStory = function() {
-		
-		
-		vm.message = '';
+    vm.createStory = function() {
+        vm.message = '';
 
-		Story.create(vm.storyData) 
-		.success(function(data) {
-			
-			
-				// clear the form
-				vm.storyData = {}
-				vm.message = data.message;
+        Story.create(vm.storyData) 
+        .success(function(data) {
+            // clear the form
+            vm.storyData = {}
+            vm.message = data.message;
+        });
+    };
 
-				
-			});
-
-		socketio.on('story', function (data) {
-				vm.stories.push(data);
-		});
-				
-	};
-
+    socketio.on('story', function (data) {
+        vm.stories.push(data);
+    });
 })
 
 
-.controller('AllStoryController', function(Story) {
+.controller('AllStoryController', function(Story, socketio) {
 
 	var vm = this;
 
 	Story.allStories()
 	.success(function(data) {
 		vm.stories = data;
-	})
+	});
+
+	 socketio.on('story', function (data) {
+        vm.stories.push(data);
+    });
 
 })
 
